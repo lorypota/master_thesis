a4eaa66ea478a414492b64f0f671d09472b41dc3
Lorenzo Rota <lorenzokaide@gmail.com>
2026-02-11 15:03:05 +0100

    Refractor actions

--- av_actions.py
+++ av_actions.py
@@ -1,29 +1,9 @@
-def available_actions(state):
-    if state[0] < 5:
-        actions = [0, 5, 10, 15, 20, 25, 30]
-    elif state[0] < 10:
-        actions = [-5, 0, 5, 10, 15, 20, 25, 30]
-    elif state[0] < 15:
-        actions = [-10, -5, 0, 5, 10, 15, 20, 25, 30]
-    elif state[0] < 20:
-        actions = [-15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
-    elif state[0] < 25:
-        actions = [-20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
-    elif state[0] < 30:
-        actions = [-25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
-    elif state[0] <= 70:
-        actions = [-30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
-    elif state[0] <= 75:
-        actions = [-30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25]
-    elif state[0] <= 80:
-        actions = [-30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20]
-    elif state[0] <= 85:
-        actions = [-30, -25, -20, -15, -10, -5, 0, 5, 10, 15]
-    elif state[0] <= 90:
-        actions = [-30, -25, -20, -15, -10, -5, 0, 5, 10]
-    elif state[0] <= 95:
-        actions = [-30, -25, -20, -15, -10, -5, 0, 5]
-    else:
-        actions = [-30, -25, -20, -15, -10, -5, 0]
+STEP = 5
+MAX_ACTION = 30
+CAPACITY = 100
 
-    return actions
+
+def available_actions(state):
+    min_action = max(-MAX_ACTION, -(state[0] // STEP) * STEP)
+    max_action = min(MAX_ACTION, ((CAPACITY - state[0]) // STEP) * STEP)
+    return list(range(min_action, max_action + STEP, STEP))
